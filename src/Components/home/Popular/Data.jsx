64e7108cf772ea25514169786aadb7568ae3767d
Alaa Yasser <alaayasser2018@gmail.com>
2023-03-05 01:25:01 +0200

    add Popular Component

---
+++ src/Components/home/Popular/Data.jsx
@@ -0,0 +1,98 @@
+import burger1 from '../../../assets/product_01.1.jpg'
+import burger2 from '../../../assets/product_01.3.jpg'
+import burger3 from '../../../assets/product_01.jpg'
+import burger4 from '../../../assets/product_01.jpg'
+import burger5 from '../../../assets/product_08.jpg'
+import burger6 from '../../../assets/product_09.jpg'
+
+import pizza1 from '../../../assets/product_2.1.jpg'
+import pizza2 from '../../../assets/product_2.2.jpg'
+import pizza3 from '../../../assets/product_2.2.jpg'
+import pizza4 from '../../../assets/product_3.2.jpg'
+import pizza5 from '../../../assets/product_3.3.jpg'
+
+import bread2 from '../../../assets/bread1.png'
+import bread3 from '../../../assets/bread2.png'
+import bread4 from '../../../assets/bread3.png'
+
+
+export const Data = [
+    {
+        id: 1,
+        image: burger1,
+        title: "Burger",
+        name: "Chicken Burger"
+    }, {
+        id: 11,
+        image: pizza5,
+        title: "Pizza",
+        name: "Thin Cheese Pizza"
+    },
+    {
+        id: 2,
+        image: burger2,
+        title: "Burger",
+        name: "Cheese Burger"
+    }, {
+        id: 12,
+        image: bread2,
+        title: "Bread",
+        name: "Crunchy Bread"
+
+    }, {
+        id: 5,
+        image: burger5,
+        title: "Burger",
+        name: "Chicken Burger"
+    }, {
+        id: 6,
+        image: burger6,
+        title: "Burger",
+        name: "Meat Burger"
+    },
+    {
+        id: 7,
+        image: pizza1,
+        title: "Pizza",
+        name: "Vegetarian Pizza"
+    },
+    {
+        id: 8,
+        image: pizza2,
+        title: "Pizza",
+        name: "Seafood Pizza"
+    }, {
+        id: 14,
+        image: bread4,
+        title: "Bread",
+        title: "Loaf Bread",
+    }, {
+        id: 9,
+        image: pizza3,
+        title: "Pizza",
+        name: "Meat Pizza"
+
+    }, {
+        id: 4,
+        image: burger4,
+        title: "Burger",
+        name: "Classic Burger"
+    }, {
+        id: 10,
+        image: pizza4,
+        title: "Pizza",
+        name: "Vegetarian Pizza"
+
+    }, {
+        id: 3,
+        image: burger3,
+        title: "Burger",
+        name: "Royal Cheese Burger"
+    },
+    {
+        id: 13,
+        image: bread3,
+        title: "Bread",
+        name: "Delicious Bread"
+    },
+];
--- src/Components/home/Popular/Popular.jsx
+++ src/Components/home/Popular/Popular.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './popular.css'
+import { Container, Row, Col } from 'react-bootstrap';
+import { Data } from './Data'
+import { GiHamburger, GiFullPizza, GiBread } from 'react-icons/gi'
 const Popular = () => {
+  const [typeFood, setTypeFood] = useState(Data);
+  const [newTypeFood, setNewTypeFood] = useState(Data);
+  const [activeType, setActiveType] = useState(0)
+  const Filter = (type) => {
+
+    let Foods = typeFood.filter(item => {
+      return (item.title == type)
+    })
+    setNewTypeFood(Foods)
+  }
   return (
-    <div>Popular</div>
+    <section className="popular">
+      <Container>
+        <h2 className="popular__title">Popular Foods</h2>
+        <ul className="Popular__filter">
+          <li onClick={() => { setNewTypeFood(Data); setActiveType(0) }} className={activeType === 0 ? 'active__type' : "popular__filter-list"}>All</li>
+          <li onClick={() => { Filter("Burger"); setActiveType(1) }} className={activeType === 1 ? 'active__type' : "popular__filter-list"}><GiHamburger />Burger</li>
+          <li onClick={() => { Filter("Pizza"); setActiveType(2) }} className={activeType === 2 ? 'active__type' : "popular__filter-list"}><GiFullPizza />Pizza</li>
+          <li onClick={() => { Filter("Bread"); setActiveType(3) }} className={activeType === 3 ? 'active__type' : "popular__filter-list"}><GiBread />Bread</li>
+        </ul>
+        <Row className="popular__food">
+          {newTypeFood.map(({ id, image, title, name }) => {
+            return (
+              <Col lg={3} className="popular__box" key={id}>
+                <img src={image} alt={title} />
+                <h3 className='popular__food-name'>{name}</h3>
+                <div className="food__buy">
+                  <span>25$</span>
+                  <a href="#" className="button button-addToCart">Add to Cart</a>
+                </div>
+              </Col>
+            )
+          })}
+        </Row>
+
+      </Container>
+    </section>
   )
 }
 
