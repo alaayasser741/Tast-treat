ad272937d5fe0279d7fbe1a08e2453b9fee67e69
Alaa Yasser <alaayasser2018@gmail.com>
2023-03-05 18:10:33 +0200

    add testimonial component

--- src/Components/home/About/About.jsx
+++ src/Components/home/About/About.jsx
@@ -6,41 +6,41 @@ import './about.css'
 const About = () => {
   return (
     <section className="about">
-      <Container>
-        <Row className='about__content'>
-          <Col lg={6} >
-            <div className="about__img">
-              <img src={aboutImg} alt="Location" />
+    <Container>
+      <Row className='about__content'>
+        <Col lg={6} >
+          <div className="about__img">
+            <img src={aboutImg} alt="Location" />
+          </div>
+        </Col>
+        <Col lg={6} className="about__info">
+          <h3>Why <span>Tasty Treat?</span></h3>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis excepturi numquam possimus illo autem illum dicta neque qui cupiditate, optio ut dolore consequuntur assumenda sapiente temporibus adipisci facilis harum quis?</p>
+          <div className="about__feature">
+            <div className="about__feat">
+              <UilCheckCircle size="1.1rem" />
+              <span>Fresh and tasty foods</span>
             </div>
-          </Col>
-          <Col lg={6} className="about__info">
-            <h3>Why <span>Tasty Treat?</span></h3>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis excepturi numquam possimus illo autem illum dicta neque qui cupiditate, optio ut dolore consequuntur assumenda sapiente temporibus adipisci facilis harum quis?</p>
-            <div className="about__feature">
-              <div className="about__feat">
-                <UilCheckCircle size="1.1rem" />
-                <span>Fresh and tasty foods</span>
-              </div>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          <div className="about__feature">
+            <div className="about__feat">
+              <UilCheckCircle size="1.1rem"/>
+              <span>Quality support</span>
             </div>
-            <div className="about__feature">
-              <div className="about__feat">
-                <UilCheckCircle size="1.1rem"/>
-                <span>Quality support</span>
-              </div>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          <div className="about__feature">
+            <div className="about__feat">
+              <UilCheckCircle size="1.1rem"/>
+              <span>Order from any location</span>
             </div>
-            <div className="about__feature">
-              <div className="about__feat">
-                <UilCheckCircle size="1.1rem"/>
-                <span>Order from any location</span>
-              </div>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </section>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  </section>
   )
 }
 
--- src/Components/home/Testimonial/Testimonial.jsx
+++ src/Components/home/Testimonial/Testimonial.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import { UilCheckCircle } from '@iconscout/react-unicons'
+import { Container, Row, Col } from 'react-bootstrap'
+import testimonialImg from '../../../assets/network.png'
+import profileImg from '../../../assets/ava-1.jpg'
+import './testimonial.css'
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
+    <section className="about">
+      <Container>
+        <Row className='about__content testimonial__content'>
+          <Col lg={6} className="about__info">
+            <span className="testimonial__title">Testimonial</span>
+            <h3>What our <span>customer</span> are saying</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis excepturi numquam possimus illo autem illum dicta neque qui cupiditate, optio ut dolore consequuntur assumenda sapiente temporibus adipisci facilis harum quis?</p>
+            <div className="testimonial__profile">
+              <img src={profileImg} alt="Profileimg" />
+              <h4>Alaa Yasser</h4>
+            </div>
+          </Col>
+          <Col lg={6} >
+            <div className="about__img testimonial__img">
+              <img src={testimonialImg} alt="Location" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
